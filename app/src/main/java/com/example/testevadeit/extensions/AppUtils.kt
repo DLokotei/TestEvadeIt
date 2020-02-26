e798bbdac79aef76f23648cff47a837a6cb9e297
DLokotei <55785543+DLokotei@users.noreply.github.com>
2020-02-26 16:05:52 +0200

    * fixed Rect.collided condition

--- app/src/main/java/com/example/testevadeit/extensions/AppUtils.kt
+++ app/src/main/java/com/example/testevadeit/extensions/AppUtils.kt
@@ -4,7 +4,6 @@ import android.graphics.Rect
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import com.example.testevadeit.MainActivity
-import java.util.*
 
 fun Fragment.getNavigation(): NavController = (activity as MainActivity).navigation
 
@@ -12,14 +11,8 @@ fun Rect.collided(target: Rect): Boolean {
     if (target.right < this.left || target.left > this.right) {
         return false
     }
-    return target.top >= this.bottom && target.bottom <= this.bottom
-}
-
-fun <T> Array<T>.replace(list: Array<T>) {
-    this.forEachIndexed { i, _ ->
-        this[i] = list[i]
+    if (target.top > this.bottom || target.bottom < this.top) {
+        return false
     }
+    return true
 }
-
-
-
